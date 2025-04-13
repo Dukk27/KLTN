7f29a9064482442190cf1e7cd147afcc16ec25a2
Dukk27 <dn596209@gmail.com>
2025-04-13 12:37:59 +0700

    Update home index

--- obj/Debug/net8.0/KLTN.AssemblyInfo.cs
+++ obj/Debug/net8.0/KLTN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KLTN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e21462e82c119d3c08d4ec3825ee1caf1dcca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b279af94c4e865830c0617ff806e088c0fab0585")]
 [assembly: System.Reflection.AssemblyProductAttribute("KLTN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KLTN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
